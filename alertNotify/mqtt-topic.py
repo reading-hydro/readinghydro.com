a51a1e8c078646b2c2ea7c4bb2ff49b41e82563b
Stuart Ward <stuart.ward@bcs.org>
2021-09-30 12:16:41 +0100

    adding no data alerts function

--- alertNotify/mqtt-topic.py
+++ alertNotify/mqtt-topic.py
@@ -1,11 +1,13 @@
 #! /usr/bin/python3
 import json
 import paho.mqtt.client as mqtt
-import time, datetime, threading, cgi
+import time, datetime
+import threading
 from calendarread import calendarread
 from restServer import restServer
 from tokenHandeler import generate_token, expired_token
 from sendmail import sendMail_alert, sendMail_shift, sendMail_esclate
+from urllib import request
 
 # Contacts hard coded for the moment need to add this in a database.
 
@@ -84,6 +86,8 @@ client.on_log = on_log
 client.tls_set("/etc/ssl/certs/ca-certificates.crt")
 who_is_oncall = {'primary' : '', 'second': ''}
 
+next_data_report_time = datetime.datetime.now()
+
 google_api_key = get_secret('GOOGLE_API_KEY')
 
 try:
@@ -104,8 +108,12 @@ restThread.start()
 
 try:
     while True:
-        if last_hour != datetime.datetime.now().hour:
-            last_hour = datetime.datetime.now().hour
+        now_utc = datetime.datetime.utcnow()
+        now = datetime.datetime.now()
+        now_utc_string = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        now_string = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        if last_hour != now.hour:
+            last_hour = now.hour
             new_who_is_oncall = calendarread(google_api_key)
             for role in ('primary', 'second'):
                 for person in new_who_is_oncall:
@@ -122,7 +130,25 @@ try:
             token = generate_token('alerts@readinghydro.org','Esculate: '+entry.get('message'), datetime.timedelta(seconds=5*60))
             sendMail_esclate('alerts@readinghydro.org', 'Esculate: '+entry.get('message'), token)
 
-        time.sleep(1)
+# check the data feeds to see if we have current data, if not raise an alert
+        latest_request = request.urlopen('https://readinghydro.org:9445/api/plc/current')
+        latest_data = json.loads(latest_request.read())
+        latest_data_time = datetime.datetime.strptime(latest_data.get('received_at'), '%Y-%m-%dT%H:%M:%S.%fZ')
+        print('That is {minutes:.2} Minutes ago'.format(minutes=(now_utc-latest_data_time).seconds/60), next_data_report_time)
+        if latest_data_time < now_utc - datetime.timedelta(seconds=20*60):
+            if now > next_data_report_time:
+                next_data_report_time = now + datetime.timedelta(seconds=15*60)
+                email1 = contacts.get(who_is_oncall.get('primary')).get('email')
+                email2 = contacts.get(who_is_oncall.get('second')).get('email')
+                alertMessage = 'No data recieved since '+latest_data_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+                alertMessage += ' That is {minutes:.2} Minutes ago'.format(minutes=(now_utc-latest_data_time).seconds/60)
+                token = generate_token(email1, 'At: {time} message: {message}'.format(time=now_string, message=alertMessage), datetime.timedelta(seconds=5*60))
+                sendMail_alert(email1,alertMessage,now_string, token)
+                sendMail_alert(email2,alertMessage,now_string, token)
+
+
+
+        time.sleep(30)
         pass
 
 except KeyboardInterrupt:
