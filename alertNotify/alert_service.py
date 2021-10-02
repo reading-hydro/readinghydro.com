bc820c14f1f6f66e99ca660d9af2b0c811493a23
Stuart Ward <stuart.ward@bcs.org>
2021-10-02 10:08:04 +0100

    adding the repeate count to the alert list

--- alertNotify/alert_service.py
+++ alertNotify/alert_service.py
@@ -155,7 +155,8 @@ _ack_resp_ok = '''\
    <body>
      <h1>Acknowlegement of alert Sucessful</h1>
      <h1>Acknowlegement alerts currently active</h1>
-     <table><tr><th>Token</th><th>Email address</th><th>Expiary Time</th><th>Status</th><th>Message</th></tr>'''
+     <table><tr><th>Token</th><th>Email address</th><th>Expiary Time</th><th>Status</th><th>Count</th><th>Message</th></tr>
+     '''
 
 _ack_resp_fail = '''\
 <html>
@@ -165,7 +166,8 @@ _ack_resp_fail = '''\
    <body>
      <h1>Acknowlegement of alert Failed</h1>
      <h1>Acknowlegement alerts currently active</h1>
-     <table><tr><th>Token</th><th>Email address</th><th>Expiary Time</th><th>Status</th><th>Message</th></tr>'''
+     <table><tr><th>Token</th><th>Email address</th><th>Expiary Time</th><th>Status</th><th>Count</th><th>Message</th></tr>
+     '''
 
 def ackresp(environ, start_response):
     start_response('200 OK', [ ('Content-type', 'text/html')])
@@ -179,7 +181,7 @@ def ackresp(environ, start_response):
         status='live'
         if entry.get('ack'): status='Acknowleged'
         resp = resp + _ack_list_body.format(token=entry.get('token'), email=entry.get('email'), 
-                                            time=entry.get('time'), status=status, message=entry.get('message'))
+                                            time=entry.get('time'), status=status, count=entry.get('count'), message=entry.get('message'))
     resp = resp + _ack_list_tail
     yield resp.encode('utf-8')
 
@@ -190,10 +192,11 @@ _ack_list_head = '''\
    </head>
    <body>
      <h1>Acknowlegement alerts currently active</h1>
-     <table><tr><th>Token</th><th>Email address</th><th>Expiary Time</th><th>Status</th><th>Message</th></tr>'''
+     <table><tr><th>Token</th><th>Email address</th><th>Expiary Time</th><th>Status</th><th>Count</th><th>Message</th></tr>
+     '''
 _ack_list_body = '''\
     <tr><td><a href="http://readinghydro.org:8080/ackresp?token={token}">Token</a></td><td>{email}</td>
-    <td>{time}</td><td>{status}</td><td>{message}</td></tr>
+    <td>{time}</td><td>{status}</td><td>{count}</td><td>{message}</td></tr>
     '''
 _ack_list_tail = '''\
     </table>
@@ -208,7 +211,7 @@ def alertlist(environ, start_response):
         status='live'
         if entry.get('ack'): status='Acknowleged'
         resp = resp + _ack_list_body.format(token=entry.get('token'), email=entry.get('email'), 
-                                            time=entry.get('time'), status=status, message=entry.get('message'))
+                                            time=entry.get('time'), status=status, count=entry.get('count'), message=entry.get('message'))
     resp = resp + _ack_list_tail
     yield resp.encode('utf-8')
 
