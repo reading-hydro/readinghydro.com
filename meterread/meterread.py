f002d79b8ee650d1786c3457b00a1fb157bdda36
Stuart Ward <stuart.ward@bcs.org>
2022-02-11 14:37:21 +0000

    Updates to meteread now only attempts to read 3 times

--- meterread/meterread.py
+++ meterread/meterread.py
@@ -25,15 +25,21 @@ def messageverify(message, checkc) -> bool:
 def main():
 
     messageelement = re.compile('1-0:([\d.]+)\\*255\\(([\d.]+)')
-    nomessage = True
+    goodmessage = False
+    maxtries = 3
 
 # Connect to the meter via the serial interface and wait till we recieve a valid message
 # message starts with a STX and ends with ETX and check character
 # we retry reading till we get a good message
 
-    portcon = serial.Serial('/dev/ttyUSB0', baudrate=9600, bytesize=7, parity='E', timeout=5)
-    while nomessage:
+    try: 
+        portcon = serial.Serial('/dev/ttyUSB0', baudrate=9600, bytesize=7, parity='E', timeout=5)
+    except serial.serialutil.SerialException:
+        print('< unable to open serial port ttyUSB0')
+        return 404
+    while maxtries > 0:
         portcon.write(b'/?!\r\n')
+        maxtries-=1
         try:
             message = portcon.read(size=1024)
         except serial.serialutil.SerialException:
@@ -47,7 +53,8 @@ def main():
                     if me2:
                         if messageverify(me2+ETX, chk):
                             print('< Message OK', file=sys.stderr)
-                            nomessage = False
+                            goodmessage = True
+                            maxtries = 0
                             portcon.write(ACK)
                         else:
                             print('< Message NOT OK', file=sys.stderr)
@@ -58,15 +65,18 @@ def main():
     readingtime = ''
     readingimport = ''
     readingexport = ''
-    elements = messageelement.findall(me2.decode())
-    jsonstring = '"date":"{isodate}","import":{rimport},"export":{rexport}'
-    for ele in elements:
-        if ele[0] == '0.9.2': readingdate=ele[1]
-        elif ele[0] == '0.9.1': readingtime=ele[1]
-        elif ele[0] == '1.8.0': readingimport=ele[1]
-        elif ele[0] == '2.8.0': readingexport=ele[1]
-    isodate = '2' + readingdate[:3] + '-' + readingdate[3:5] + '-' + readingdate[5:7] + 'T' + readingtime[:2] + ':' + readingtime[2:4] + ':' + readingtime[4:6] + 'Z'
-    print('{' + jsonstring.format(isodate=isodate, rimport=readingimport, rexport=readingexport) + '}')
+    if goodmessage:
+        elements = messageelement.findall(me2.decode())
+        jsonstring = '"date":"{isodate}","import":{rimport},"export":{rexport}'
+        for ele in elements:
+            if ele[0] == '0.9.2': readingdate=ele[1]
+            elif ele[0] == '0.9.1': readingtime=ele[1]
+            elif ele[0] == '1.8.0': readingimport=ele[1]
+            elif ele[0] == '2.8.0': readingexport=ele[1]
+        isodate = '2' + readingdate[:3] + '-' + readingdate[3:5] + '-' + readingdate[5:7] + 'T' + readingtime[:2] + ':' + readingtime[2:4] + ':' + readingtime[4:6] + 'Z'
+        print('{' + jsonstring.format(isodate=isodate, rimport=readingimport, rexport=readingexport) + '}')
+    else:
+        print('< Failed to read meter after 3 attempts')
     return
 
 #  b'/ISk5MT174-0002\r\n\x021-0:0.9.2*255(0220127)\r\n1-0:0.9.1*255(105644)\r\n1-0:1.8.0*255(0000502.853*kWh)\r\n1-0:2.8.0*255(0098697.318*kWh)\r\n!\r\n\x03\x19'
