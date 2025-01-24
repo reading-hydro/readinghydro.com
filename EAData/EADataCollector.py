270cde3cf5213415718df03488dcb9dd71e20863
Stuart Ward <stuart.ward@bcs.org>
2025-01-24 12:34:04 +0000

    correct record count on EA Data download

--- EAData/EADataCollector.py
+++ EAData/EADataCollector.py
@@ -32,6 +32,7 @@ def get_secret(MySecretString):
 def main():
 
     syslog.syslog("Starting update of EA data tables")
+    print("Starting update of EA data tables")
 
     DbHost = get_secret("DB_HOST")
     DbPort = int(get_secret("DB_PORT"))
@@ -43,14 +44,15 @@ def main():
         db = psycopg2.connect(dbname=DbName, user=DbUser, password=DbPassword, host=DbHost, port=DbPort)
     except Exception as e:
         print("Database error on connection: ",e)
-        syslog.syslog("Database connection error")
+        syslog.syslog("Database connection error",e)
 
 # get the datetime of the last database entry
     
     cursor = db.cursor()
-    cursor.execute("SELECT time FROM public.eadata ORDER BY entrytime DESC")
-    row = cursor.fetchone()[0]
-    LastEntry = row.entrytime
+    cursor.execute("SELECT entrytime FROM public.eadata ORDER BY entrytime DESC")
+    LastEntry = cursor.fetchone()[0]
+    syslog.syslog("Last data entry is currently: " + LastEntry.strftime("%Y=%m-%d %H:%M:%S"))
+    print("Last data entry is currently: ", LastEntry)
 
 # get the flow data from the Enviroment Agency since last entry
     myds3 = LastEntry.strftime('%Y-%m-%d')
@@ -63,14 +65,20 @@ def main():
         syslog.syslog("Error getting EA data")
 # enter the data into the database only if is after the LsatEntry (new data)
 
+    rowsadded = 0
     data = json.loads(jsondata.read())
     items = data.get('items')
     for row in items:
-        if row.get('datetime') > LastEntry:
-#            cursor.execute("INSERT INTO public.eadata (entrytime, flow) VALUES (%s, %s)", (row.get('dateTime'), row.get('value')))
-            print(row.get('dateTime'), row.get('value'))
+        rowtime = datetime.datetime.strptime(row.get('dateTime'),"%Y-%m-%dT%H:%M:%SZ")
+        rowtime = rowtime.replace(tzinfo=datetime.timezone.utc)
+        if  rowtime > LastEntry:
+            cursor.execute("INSERT INTO public.eadata (entrytime, flow) VALUES (%s, %s)", (row.get('dateTime'), row.get('value')))
+            rowsadded += 1
+
 # commit and close the database
-    
+
+    syslog.syslog("Rows added are: " + str(rowsadded))
+    print("Rows added are: ",rowsadded)    
     db.commit()
     cursor.close()
     db.close()
