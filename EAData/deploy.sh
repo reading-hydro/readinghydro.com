d6b209fe8d358706deb2d8b1eb8fe02227852111
Stuart Ward <stuart.ward@bcs.org>
2026-01-09 14:59:32 +0000

    Add deployment of the cron script to the EAData deploy script

--- EAData/deploy.sh
+++ EAData/deploy.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
 sudo cp EADataCollector.py /usr/local/bin/EADataCollector.py
+sudo cp eadatacron /etc/cron.hourly/eadatacron
+sudo chmod +x /usr/local/bin/EADataCollector.py
+sudo chmod +x /etc/cron.hourly/eadatacron
 
