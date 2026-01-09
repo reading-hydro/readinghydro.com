482e60dcc11f7d7ae5a554cf34573273af911899
Stuart Ward <stuart.ward@bcs.org>
2026-01-09 15:23:41 +0000

    change the letsencrypt deploy script to copy the latest certs for mosquitto to use as this starts as the mosquitto user and cannot access the cert file in the letsencrypt directory

--- etc/letsencrypt/renewal-hooks/deploy/readinghydro.org.deploy.sh
+++ etc/letsencrypt/renewal-hooks/deploy/readinghydro.org.deploy.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
+# Script to restart services after Let's Encrypt certificate renewal
+#
+# copy the certifacte files for the mosquitto broker
+cp -L /etc/letsencrypt/live/readinghydro.org/cert.pem /etc/mosquitto/certs/cert.pem
+cp -L /etc/letsencrypt/live/readinghydro.org/privkey.pem /etc/mosquitto/certs/privkey.pem
+cp -L /etc/letsencrypt/live/readinghydro.org/chain.pem /etc/mosquitto/certs/chain.pem
+chown mosquitto:mosquitto /etc/mosquitto/certs/cert.pem
+chown mosquitto:mosquitto /etc/mosquitto/certs/privkey.pem
+chown mosquitto:mosquitto /etc/mosquitto/certs/chain.pem
+chmod 640 /etc/mosquitto/certs/cert.pem
+chmod 640 /etc/mosquitto/certs/privkey.pem
+chmod 640 /etc/mosquitto/certs/chain.pem
+# restart services to pick up the new certificates
 systemctl restart hydro-api.service
 systemctl restart mosquitto.service
 systemctl restart apache2.service
